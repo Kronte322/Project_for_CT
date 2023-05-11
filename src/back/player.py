e76b2f9bc8ee737d3116d0f84136181afb9a9a5c
guljamov rahim <guliamov.rn@phystech.edu>
2023-05-11 16:31:12 +0300

    center

--- src/back/player.py
+++ src/back/player.py
@@ -136,9 +136,9 @@ class Player:
 
         if self.direction[1] < 0:
             if not mappa.CanStandThere(
-                    (self.rect.x, self.rect.y + self.direction[1])) or not mappa.CanStandThere(
+                    (self.rect.x, self.rect.y + kSizeOfCharacter // 2 + self.direction[1])) or not mappa.CanStandThere(
                 (self.rect.x + kSizeOfCharacter,
-                 self.rect.y + self.direction[1])):
+                 self.rect.y + kSizeOfCharacter // 2 + self.direction[1])):
                 self.direction[1] = 0
         elif self.direction[1] > 0:
             if not mappa.CanStandThere(
